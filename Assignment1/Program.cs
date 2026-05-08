ae3c883ca3d786d5b09edf164102251873cc74e0
abashidzeluka <abashidze2003@gmail.com>
2026-05-08 22:57:14 +0400

    Initial commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();        }
+    }
+}
